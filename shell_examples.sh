582307f4d691e917b6df5557ab172bbdc2b1e16e
krisypon@inbox.lv <jelizavetak@inbox.lv>
2017-09-28 09:51:45 +0300

    28170921_09_50

---
+++ shell_examples.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#1.piemērs
+#NAME="Vārds Uzvārds"
+#echo $NAME
+
+#2.piemērs
+#NAME="Vārds Uzvārds"
+#readonly NAME
+#echo $NAME
+#NAME="Uzvārds Vārds"
+#echo $NAME
+
+#3.piemērs
+#NAME="Vārds Uzvārds"
+#echo $NAME
+#unset NAME
+#echo $NAME
+
+#4.piemērs
+echo "Atbilde uz jautājumu - kas šobrīd tik pildīts? "$0
+#echo $n
+echo "Skriptam nodoto argumentu skaits: " $#
+
+
+
+
