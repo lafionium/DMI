c4cb846536ac6552a3dc0125891718b999fd5ca3
krisypon@inbox.lv <jelizavetak@inbox.lv>
2017-11-02 11:55:20 +0200

    20172610_11_55

---
+++ PYTHON/hello.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#print "Hello World!"
+
+'''
+print "Here \bthe \bspaces \bare \bbackspaced."
+raksta bez atstarpem      \b peremesaet kursor nazad
+'''
+
+'''
+print "Here \nthe \nspaces \nare \nnewlined."
+piwet kazdoje slovo s novoj strocki \n
+'''
+
+'''
+print "Here \tthe \tspaces \thave \thorizontal \ttab \tspaces."
+piwet vmesto probela TAB
+hf	|
+ghf	|
+jghhf	|
+'''
+
+'''
+print "Here \vthe \vspaces \vhave \vvertical \vtab \vspaces."
+Here
+     the
+         spaces
+                have
+                     vertical
+                              tab
+                                  spaces.
+'''
+                                                                                                                                         
+print "Сижу \vза \vрешеткой \vв \vтемнице \vсырой.\vВскормленный \vв \vневоле \vорел \vмолодой,"
+print"\t\t\t\t\t\t\t\tМой \n\t\t\t\t\t\t\tгрустный \n\t\t\t\t\t\tтоварищ, \n\t\t\t\t\tмахая \n\t\t\t\tкрылом,\n\t\t\tКровавую \n\t\tпищу \n\tклюет \nпод окном,"
