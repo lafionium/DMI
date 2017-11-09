62bc3234e5a5fbef3f5b855d55ad2089f5c81411
krisypon@inbox.lv <jelizavetak@inbox.lv>
2017-11-09 09:54:19 +0200

    20172610_09_55

---
+++ PYTHON/dialogs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+'''
+a = input("Lietotaj lūdzi ievadi kaut ko:")
+print "Tu ievadi lielumu ar %s dat tipu"%(type(a))
+print a * a
+print a + a
+
+a = raw_input("Lietotaj lūdzi ievadi kaut ko:")
+print "Tu ievadi lielumu ar %s dat tipu"%(type(a))
+#print a * a
+print a + a
+'''
+
+a = raw_input("Lietotājs ievadi savu vārdu: ")
+b = raw_input("Lietotājs ievadi savu uzvārdu: ")
+print "Vanga saka ka tevi sauc - %s "%(a + ' ' + b)
+                                     #(a + ahr(32) + b)
---
+++ PYTHON/variables.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+a = 65
+print type(a)
+print "Mainīga a vētrība kā dec. skaitlis ir: %d"%(a)
+print "Mainīga a vētrība kā hec. skaitlis ir: %x"%(a)
+print "Mainīga a vētrība kā oct. skaitlis ir: %o"%(a)
+print "Mainīga a vētrība kā simbols ir: %c"%(a)
+a = 'A'
+print type(a)
+print "Mainīga a vētrība kā dec. skaitlis ir: %d"%(ord(a))
+print "Mainīga a vētrība kā hec. skaitlis ir: %x"%(ord(a))
+print "Mainīga a vētrība kā oct. skaitlis ir: %o"%(ord(a))
+print "Mainīga a vētrība kā simbols ir: %c"%(a)
+a = '\n'
+print type(a)
+print "Mainīga a vētrība kā dec. skaitlis ir: %d"%(ord(a))
+print "Mainīga a vētrība kā hec. skaitlis ir: %x"%(ord(a))
+print "Mainīga a vētrība kā oct. skaitlis ir: %o"%(ord(a))
+print "Mainīga a vētrība kā simbols ir: %c"%(a)
+a = 8.5
+print type(a)
+print "Mainīga a vētrība kā dec. skaitlis ir: %d"%(a)
+print "Mainīga a vētrība kā hec. skaitlis ir: %x"%(a)
+print "Mainīga a vētrība kā oct. skaitlis ir: %o"%(a)
+#print "Mainīga a vētrība kā simbols ir: %c"%(a)
+print "Mainīga a vētrība kā oct. skaitlis ir: %10.3f"%(a)
+
+
