20a4b70942bcd848f8c3acf07c118f14e3ada553
krisypon@inbox.lv <jelizavetak@inbox.lv>
2017-12-14 12:05:38 +0200

    201731412_12_05

--- PYTHON/tabula.py
+++ PYTHON/tabula.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bi/python
 # -*- coding: utf-8 -*-
 a = input ("Lietotājs, lūdzu bistro ievadi sākuma skaitli:")
              
@@ -13,3 +13,10 @@ print a + 7
 print a + 8
 print a + 9
 print a + 10
+
+for a in range(skaitlis, skaitlis+11, 1):
+	print "%d \t"%(x),
+	for i in range(1, 6, 1):
+		sk_mod=x%i
+		print "%d \t"%(sk_mod),
+print "\n"
